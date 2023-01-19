c593ec716328e6edca514ceca13a90219ef8868d
faringet <mik.davydov14@gmail.com>
2023-01-19 22:05:42 +0300

    initializers added

--- main.go
+++ main.go
@@ -1,17 +1,12 @@
 package main
 
 import (
+	"January_1st_Calculator/initializers"
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-	"log"
 )
 
 func init() {
-	err := godotenv.Load()
-
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	initializers.LoadEnvVariables()
 }
 
 func main() {
