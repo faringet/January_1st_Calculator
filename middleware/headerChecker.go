5f5151b0d941442f6afdaf6ea63cf44375592599
Mikhail Davydov <104436545+faringet@users.noreply.github.com>
2023-01-23 12:15:31 +0300

    PingMiddleware update

--- middleware/headerChecker.go
+++ middleware/headerChecker.go
@@ -8,14 +8,13 @@ import (
 func PingMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		logger := logging.GetLogger()
-		// Check if the X-PING header is present in the request
+		// Проверка наличие заголовка X-PING в запросе
 		ping := c.Request.Header.Get("X-PING")
 		if ping != "" {
-			// Add the X-PONG header to the response
+			// Добавляем в ответ заголовок X-PONG.
 			c.Header("X-PONG", "pong")
 			logger.Info("HTTP header contains X-PING")
 		}
-		// Proceed to the next middleware or main handler
 		c.Next()
 	}
 }
